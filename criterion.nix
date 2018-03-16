1d553fb8045f8317842240f8b53fb3e074887293
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-03-15 20:46:34 -0700

    Increase lower bound on `criterion`
    Fixes #21

--- criterion.nix
+++ criterion.nix
@@ -1,6 +1,6 @@
 { mkDerivation, aeson, ansi-wl-pprint, base, base-compat, binary
 , bytestring, cassava, code-page, containers, deepseq, directory
-, exceptions, fetchgit, filepath, Glob, HUnit, js-flot, js-jquery
+, exceptions, filepath, Glob, HUnit, js-flot, js-jquery
 , microstache, mtl, mwc-random, optparse-applicative, parsec
 , QuickCheck, semigroups, statistics, stdenv, tasty, tasty-hunit
 , tasty-quickcheck, text, time, transformers, transformers-compat
@@ -8,14 +8,13 @@
 }:
 mkDerivation {
   pname = "criterion";
-  version = "1.2.4.0";
-  src = fetchgit {
-    url = "https://github.com/bos/criterion";
-    sha256 = "0ir02sgfr40y77bh5q1sajld387bjkwl5gdlc7cwz9xcyadk8019";
-    rev = "bea91eae7a6c0d4246045f27c9462ea6d138591a";
-  };
+  version = "1.4.0.0";
+  sha256 = "cc65c71c519372a28aa7a715da95038bc2be5a1ecbd9c1e26408876f06c61a6a";
+  revision = "1";
+  editedCabalFile = "056p7d95ynrpsm5jr479r9xk7ksniqkz4bza0zdn9a8vmrx591qh";
   isLibrary = true;
   isExecutable = true;
+  enableSeparateDataOutput = true;
   libraryHaskellDepends = [
     aeson ansi-wl-pprint base base-compat binary bytestring cassava
     code-page containers deepseq directory exceptions filepath Glob
@@ -23,10 +22,12 @@ mkDerivation {
     parsec semigroups statistics text time transformers
     transformers-compat vector vector-algorithms
   ];
-  executableHaskellDepends = [ base optparse-applicative ];
+  executableHaskellDepends = [
+    base base-compat optparse-applicative semigroups
+  ];
   testHaskellDepends = [
-    aeson base bytestring deepseq directory HUnit QuickCheck statistics
-    tasty tasty-hunit tasty-quickcheck vector
+    aeson base base-compat bytestring deepseq directory HUnit
+    QuickCheck statistics tasty tasty-hunit tasty-quickcheck vector
   ];
   homepage = "http://www.serpentine.com/criterion";
   description = "Robust, reliable performance measurement and analysis";
