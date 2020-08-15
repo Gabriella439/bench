1434fb4e7f9708f1cc4691ae3005ccf6df319e47
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-08-15 11:04:43 -0700

    Build against `optparse-applicative-0.16.0.0`

--- release.nix
+++ release.nix
@@ -1,12 +1,8 @@
 let
-  fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOs/nixpkgs/archive/d8e0ade97ad89cd7ea4452e41b4abcaf7e04a8b7.tar.gz";
 
-  nixpkgs = fetchNixpkgs {
-    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
-
-    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
-
-    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+    sha256 = "1rm6z9cch0kld1742inpsch06n97qik30a3njglvq52l8g9xw2jj";
   };
 
   readDirectory = import ./nix/readDirectory.nix;
@@ -25,13 +21,15 @@ let
                     (pkgs.haskell.lib.addBuildDepend
                       (pkgs.haskell.lib.appendConfigureFlag
                         (pkgs.haskell.lib.appendConfigureFlag
-                          haskellPackagesOld.criterion_1_4_0_0
+                          haskellPackagesOld.criterion
                           "-ffast"
                         )
                         "-fembed-data-files"
                       )
                       haskellPackagesOld.file-embed
                     );
+
+                turtle = pkgs.haskell.lib.doJailbreak haskellPackagesOld.turtle;
               };
 
           in
